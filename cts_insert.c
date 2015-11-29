c9b6f72821941560304050495c8eeee88cb3fa0e
Lester Vecsey <lvecsey@gmail.com>
2015-11-28 20:43:12 -0500

    update for cmd cts_insert

--- cts_insert.c
+++ cts_insert.c
@@ -10,6 +10,8 @@
 
 #include <string.h>
 
+#include "cts_stor.h"
+
 int main(int argc, char *argv[]) {
 
   struct timespec current;
@@ -20,25 +22,35 @@ int main(int argc, char *argv[]) {
   ssize_t bytes_read;
   ssize_t bytes_written;
 
+  u_int16_t cmd;
+
+  unsigned char buf[10];
+
   long int debug = getenv("DEBUG") != NULL;
-
-  while ((bytes_read = getline(&line, &len, fp)) != -1) {
-
-    if (len>0) {
-      sscanf(line, "%ld %ld", &current.tv_sec, &current.tv_nsec);
-      bytes_written = write(7, &current, sizeof(struct timespec));
-      if (bytes_written != sizeof(struct timespec)) return -1;
-    }
-
-  }
-
-  close(1);
 
   {
     u_int64_t offset;
     u_int16_t indexno;
 
-    unsigned char buf[10];
+    cmd = CTS_INSERT;
+
+    bytes_written = write(7, &cmd, sizeof(cmd));
+    if (bytes_written != sizeof(cmd)) return -1;
+
+    while ((bytes_read = getline(&line, &len, fp)) != -1) {
+
+      if (len>0) {
+	sscanf(line, "%ld %ld", &current.tv_sec, &current.tv_nsec);
+	bytes_written = write(7, &current, sizeof(struct timespec));
+	if (bytes_written != sizeof(struct timespec)) return -1;
+      }
+
+    }
+
+    current.tv_sec = 0;
+    current.tv_nsec = 0;
+    bytes_written = write(7, &current, sizeof(struct timespec));
+    if (bytes_written != sizeof(struct timespec)) return -1;    
 
     bytes_read = read(6, buf, 10);
     if (bytes_read != 10) return -1;
